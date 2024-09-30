fd13102c38c00b1fb66ee2e7a6541b857fcc9e15
Srihariva1111 <sriharivas5@gmail.com>
2024-09-30 20:18:15 +0530

    commit changes

--- server/server.js
+++ server/server.js
@@ -2,17 +2,21 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const router = require("./route");
-const cors = require('cors')
+const cors = require("cors");
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 app.use("/api", router);
 
 const port = process.env.PORT || 3001;
 
-mongoose.connect("mongodb://localhost:27017/test").then(() => {
-  console.log("database connected successfully");
-});
+mongoose
+  .connect(
+    "mongodb+srv://Sriharivas:Nagamani.0@deploy.tidah.mongodb.net/deploy?retryWrites=true&w=majority&appName=deploy"
+  )
+  .then(() => {
+    console.log("database connected successfully");
+  });
 
 app.listen(port, () => {
   console.log(`server is succesfully running on localhost ${port}`);
